148188f15a0a34967cbf5d69245007ece10e3e72
sagar.gupta <sagar.gupta@ucalgary.ca>
2017-10-27 16:24:04 -0600

    Adding FrontPage code

---
+++ WpfApp1/WpfApp1/FrontPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class FrontPage : Window
+    {
+        public FrontPage()
+        {
+            InitializeComponent();
+
+            this.Title = "DigiCook";
+
+            // Set window to center of the computer screen at startup
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            // Turn off resizing
+            this.ResizeMode = ResizeMode.NoResize;
+        }
+
+        private void ButtonEnter(object sender, MouseEventArgs e)
+        {
+            this.button.Content = "Enter";
+        }
+
+        private void ButtonLeave(object sender, MouseEventArgs e)
+        {
+            this.button.Content = "Left";
+        }
+    }
+}
