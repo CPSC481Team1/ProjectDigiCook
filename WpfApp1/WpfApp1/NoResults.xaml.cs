27a52ef3360fc5cb607367708a2dcf1cbb9d1ee0
Sagar <sagar.gupta@ucalgary.ca>
2017-11-20 01:20:48 -0700

    Not found pages and modified category buttons

--- WpfApp1/WpfApp1/NoResults.xaml.cs
+++ WpfApp1/WpfApp1/NoResults.xaml.cs
@@ -36,15 +36,7 @@ namespace WpfApp1
             //this.ResizeMode = ResizeMode.NoResize;
         }
 
-        private void ButtonEnter(object sender, MouseEventArgs e)
-        {
-            this.button.Content = "Enter";
-        }
-
-        private void ButtonLeave(object sender, MouseEventArgs e)
-        {
-            this.button.Content = "Left";
-        }
+     
 
         private void View_Checklist_Click(object sender, RoutedEventArgs e)
         {
@@ -54,6 +46,51 @@ namespace WpfApp1
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             searchBar.setText();
+        }
+
+       
+
+        private void spaghetti_MouseEnter(object sender, MouseEventArgs e)
+        {
+            spaghetti.Width = 215;
+            spaghetti.Height = 215;
+        }
+
+       
+
+        private void spaghetti_MouseLeave(object sender, MouseEventArgs e)
+        {
+            spaghetti.Width = 200;
+            spaghetti.Height = 200;
+        }
+
+        private void lunch_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lunch.Height = 215;
+            lunch.Width = 215;
+        }
+
+        private void lunch_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lunch.Height = 200;
+            lunch.Width = 200;
+        }
+
+        private void dinner_MouseEnter(object sender, MouseEventArgs e)
+        {
+            dinner.Height = 215;
+            dinner.Width = 215;
+        }
+
+        private void dinner_MouseLeave(object sender, MouseEventArgs e)
+        {
+            dinner.Height = 200;
+            dinner.Width = 200;
+        }
+
+        private void spaghetti_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("./Spaghetti.xaml", UriKind.Relative));
         }
     }
 }
