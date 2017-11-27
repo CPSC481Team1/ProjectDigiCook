ca663d0501e2a7b508df9240332f8509b840cb22
sao <sam_ao8@hotmail.com>
2017-11-26 23:53:16 -0700

    Added fullscreen button, close button, video seeking, improved step readability and popup video to ingredients page

--- WpfApp1/WpfApp1/Recipe.xaml.cs
+++ WpfApp1/WpfApp1/Recipe.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -43,6 +44,8 @@ namespace WpfApp1
                 }
             }
         }
+        private TimeSpan TotalTime;
+        private DispatcherTimer timerVideoTime;
         private void openVideo(object sender, MouseButtonEventArgs e)
         {
             var source = ((Image)sender).Tag;
@@ -53,17 +56,69 @@ namespace WpfApp1
             play_button.Visibility = Visibility.Hidden;
             play_button.IsEnabled = false;
             pause_button.Visibility = Visibility.Visible;
-            play_button.IsEnabled = true;
+            pause_button.IsEnabled = true;
+            fullscreenclose_button.Visibility = Visibility.Hidden;
+            fullscreenclose_button.IsEnabled = false;
+            fullscreen_button.Visibility = Visibility.Visible;
+            fullscreen_button.IsEnabled = true;
+            paused = false;
         }
-        
+        private void videoOpened(object sender, RoutedEventArgs e)
+        {
+            TotalTime = Video.NaturalDuration.TimeSpan;
+            seekSlider.Maximum = TotalTime.TotalSeconds;
+            // Create a timer that will update the counters and the time slider
+            timerVideoTime = new DispatcherTimer();
+            timerVideoTime.Interval = TimeSpan.FromMilliseconds(100);
+            timerVideoTime.Tick += new EventHandler(timerTick);
+            timerVideoTime.Start();
+        }
+        bool suppressValueChanged = false;
+        bool paused = false;
+        void timerTick(object sender, EventArgs e)
+        {
+            if (paused)
+            {
+                return;
+            }
+            // Check if the movie finished calculate it's total time
+            if (Video.NaturalDuration.TimeSpan.TotalSeconds > 0)
+            {
+                if (TotalTime.TotalSeconds > 0)
+                {
+                    // Updating time slider
+                    suppressValueChanged = true;
+                    seekSlider.Value = Video.Position.TotalSeconds;
+                }
+            }
+        }
+
+        private void videoSeek(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (suppressValueChanged)
+            {
+                suppressValueChanged = false;
+            }
+            else
+            {
+                Video.Position = TimeSpan.FromSeconds(seekSlider.Value);
+            }
+        }
+
         private void videoPlayerClose(object sender, EventArgs e)
         {
+            seekSlider.Value = 0;
             Video.Stop();
+            timerVideoTime.Stop();
+            Video.Height = 333;
+            Video.Width = 591;
         }
 
         private void playButton(object sender, MouseButtonEventArgs e)
         {
             Video.Play();
+            Video.MouseLeftButtonUp += pauseButton;
+            Video.MouseLeftButtonUp -= playButton;
             play_button.Visibility = Visibility.Hidden;
             play_button.IsEnabled = false;
             pause_button.Visibility = Visibility.Visible;
@@ -71,11 +126,28 @@ namespace WpfApp1
         }
         private void pauseButton(object sender, MouseButtonEventArgs e)
         {
+            paused = false;
             Video.Pause();
+            Video.MouseLeftButtonUp += playButton;
+            Video.MouseLeftButtonUp -= pauseButton;
             pause_button.Visibility = Visibility.Hidden;
             pause_button.IsEnabled = false;
             play_button.Visibility = Visibility.Visible;
             play_button.IsEnabled = true;
+        }
+
+        private void videoEnded(object sender, RoutedEventArgs e)
+        {
+            Video.Stop();
+            pause_button.Visibility = Visibility.Hidden;
+            pause_button.IsEnabled = false;
+            play_button.Visibility = Visibility.Visible;
+            play_button.IsEnabled = true;
+        }
+
+        private void exitButton(object sender, MouseButtonEventArgs e)
+        {
+            VideoPlayer.IsOpen = false;
         }
 
         private void volumeButton(object sender, MouseButtonEventArgs e)
@@ -84,6 +156,22 @@ namespace WpfApp1
 
         private void fullscreenButton(object sender, MouseButtonEventArgs e)
         {
+            Video.Height = 900;
+            Video.Width = 1600;
+            fullscreen_button.Visibility = Visibility.Hidden;
+            fullscreen_button.IsEnabled = false;
+            fullscreenclose_button.Visibility = Visibility.Visible;
+            fullscreenclose_button.IsEnabled = true;
+        }
+
+        private void fullscreenButtonClose(object sender, MouseButtonEventArgs e)
+        {
+            Video.Height = 333;
+            Video.Width = 591;
+            fullscreenclose_button.Visibility = Visibility.Hidden;
+            fullscreenclose_button.IsEnabled = false;
+            fullscreen_button.Visibility = Visibility.Visible;
+            fullscreen_button.IsEnabled = true;
         }
 
         private void listBox_SelectionChanged(object sender, EventArgs e)
--- WpfApp1/WpfApp1/ingredients.xaml.cs
+++ WpfApp1/WpfApp1/ingredients.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -88,6 +89,136 @@ namespace WpfApp1
 
             int idx = ingredientWithAlts.IndexOf((Label)ingredientsBox.SelectedItem);
             MessageBox.Show(alts[idx], "Alternative Ingredient:");
+        }
+        private TimeSpan TotalTime;
+        private DispatcherTimer timerVideoTime;
+        private void openVideo(object sender, MouseButtonEventArgs e)
+        {
+            var source = ((Image)sender).Tag;
+            var original = Video.Source;
+            Video.Source = new Uri(source.ToString(), UriKind.Relative);
+            VideoPlayer.IsOpen = true;
+            Video.Play();
+            play_button.Visibility = Visibility.Hidden;
+            play_button.IsEnabled = false;
+            pause_button.Visibility = Visibility.Visible;
+            pause_button.IsEnabled = true;
+            fullscreenclose_button.Visibility = Visibility.Hidden;
+            fullscreenclose_button.IsEnabled = false;
+            fullscreen_button.Visibility = Visibility.Visible;
+            fullscreen_button.IsEnabled = true;
+            paused = false;
+        }
+        private void videoOpened(object sender, RoutedEventArgs e)
+        {
+            TotalTime = Video.NaturalDuration.TimeSpan;
+            seekSlider.Maximum = TotalTime.TotalSeconds;
+            // Create a timer that will update the counters and the time slider
+            timerVideoTime = new DispatcherTimer();
+            timerVideoTime.Interval = TimeSpan.FromMilliseconds(100);
+            timerVideoTime.Tick += new EventHandler(timerTick);
+            timerVideoTime.Start();
+        }
+        bool suppressValueChanged = false;
+        bool paused = false;
+        void timerTick(object sender, EventArgs e)
+        {
+            if (paused)
+            {
+                return;
+            }
+            // Check if the movie finished calculate it's total time
+            if (Video.NaturalDuration.TimeSpan.TotalSeconds > 0)
+            {
+                if (TotalTime.TotalSeconds > 0)
+                {
+                    // Updating time slider
+                    suppressValueChanged = true;
+                    seekSlider.Value = Video.Position.TotalSeconds;
+                }
+            }
+        }
+
+        private void videoSeek(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (suppressValueChanged)
+            {
+                suppressValueChanged = false;
+            }
+            else
+            {
+                Video.Position = TimeSpan.FromSeconds(seekSlider.Value);
+            }
+        }
+
+        private void videoPlayerClose(object sender, EventArgs e)
+        {
+            seekSlider.Value = 0;
+            Video.Stop();
+            timerVideoTime.Stop();
+            Video.Height = 333;
+            Video.Width = 591;
+        }
+
+        private void playButton(object sender, MouseButtonEventArgs e)
+        {
+            Video.Play();
+            Video.MouseLeftButtonUp += pauseButton;
+            Video.MouseLeftButtonUp -= playButton;
+            play_button.Visibility = Visibility.Hidden;
+            play_button.IsEnabled = false;
+            pause_button.Visibility = Visibility.Visible;
+            pause_button.IsEnabled = true;
+        }
+
+        private void pauseButton(object sender, MouseButtonEventArgs e)
+        {
+            paused = false;
+            Video.Pause();
+            Video.MouseLeftButtonUp += playButton;
+            Video.MouseLeftButtonUp -= pauseButton;
+            pause_button.Visibility = Visibility.Hidden;
+            pause_button.IsEnabled = false;
+            play_button.Visibility = Visibility.Visible;
+            play_button.IsEnabled = true;
+        }
+
+        private void videoEnded(object sender, RoutedEventArgs e)
+        {
+            Video.Stop();
+            pause_button.Visibility = Visibility.Hidden;
+            pause_button.IsEnabled = false;
+            play_button.Visibility = Visibility.Visible;
+            play_button.IsEnabled = true;
+        }
+
+        private void exitButton(object sender, MouseButtonEventArgs e)
+        {
+            VideoPlayer.IsOpen = false;
+        }
+
+        private void volumeButton(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void fullscreenButton(object sender, MouseButtonEventArgs e)
+        {
+            Video.Height = 900;
+            Video.Width = 1600;
+            fullscreen_button.Visibility = Visibility.Hidden;
+            fullscreen_button.IsEnabled = false;
+            fullscreenclose_button.Visibility = Visibility.Visible;
+            fullscreenclose_button.IsEnabled = true;
+        }
+
+        private void fullscreenButtonClose(object sender, MouseButtonEventArgs e)
+        {
+            Video.Height = 333;
+            Video.Width = 591;
+            fullscreenclose_button.Visibility = Visibility.Hidden;
+            fullscreenclose_button.IsEnabled = false;
+            fullscreen_button.Visibility = Visibility.Visible;
+            fullscreen_button.IsEnabled = true;
         }
     }
 }
